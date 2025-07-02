ae0a4b5556b9eeda42de354dd90c66e2c4434adf
lokutus24 <raskagabor22@gmail.com>
2025-07-02 15:49:38 +0200

    Add GitHub update checker

---
+++ inc/Base/MibUpdater.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin update checker for GitHub releases.
+ */
+namespace Inc\Base;
+
+class MibUpdater extends MibBaseController
+{
+    public function registerFunction()
+    {
+        add_filter('site_transient_update_plugins', array($this, 'check_update'));
+    }
+
+    public function check_update($transient)
+    {
+        if (empty($transient->checked)) {
+            return $transient;
+        }
+
+        $plugin_slug = $this->pluginName;
+        $current_version = defined('MIB_VERSION') ? MIB_VERSION : null;
+
+        if (!$current_version) {
+            return $transient;
+        }
+
+        $response = wp_remote_get('https://api.github.com/repos/lokutus24/mib/releases/latest');
+
+        if (!is_wp_error($response) && isset($response['response']['code']) && $response['response']['code'] == 200) {
+            $data = json_decode(wp_remote_retrieve_body($response));
+            if (isset($data->tag_name) && version_compare($current_version, $data->tag_name, '<')) {
+                $transient->response[$plugin_slug] = (object) array(
+                    'slug'        => dirname($plugin_slug),
+                    'plugin'      => $plugin_slug,
+                    'new_version' => $data->tag_name,
+                    'url'         => $data->html_url,
+                    'package'     => $data->zipball_url,
+                );
+            }
+        }
+
+        return $transient;
+    }
+}
